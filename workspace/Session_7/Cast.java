716abb8393e0dfebc09da3791a9ca346242f2ccc
pi <peter@isburgh.com>
2015-01-14 12:39:26 -0600

    Add comments and edits for non-generic Cast.java in Session_7

--- workspace/Session_7/Cast.java
+++ workspace/Session_7/Cast.java
@@ -3,14 +3,16 @@ import java.util.ArrayList;
 public class Cast {
 
 	public static void main(String[] args) {
-	    ArrayList<Integer> a = new ArrayList<>();
-	    a.add(3);
+	    //ArrayList<Integer> a = new ArrayList<>(); // don't need to cast generics
+	    ArrayList a = new ArrayList(); // raw ArrayList doesn't know types inside
+	    a.add("3");
 	    a.add(8);
         System.out.println("element 0 is " + a.get(0).getClass() 
         		+ " with value " + a.get(0));
         System.out.println("element 1 is " + a.get(1).getClass() 
         		+ " with value " + a.get(1));
-        Integer x = (Integer) a.get(1);
+        Integer x = (Integer) a.get(1); // have to cast to get out (even though Java
+                                        // knows what type it is!)
 	}
 
 }
